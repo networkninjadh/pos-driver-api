887ef8783e1304b959cf04972864b62a6fcc927f
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:18:23 -0500

    initial commit

---
+++ src/main/java/com/howtech/posdriverapi/PosDriverApiApplication.java
@@ -0,0 +1,13 @@
+package com.howtech.posdriverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosDriverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosDriverApiApplication.class, args);
+	}
+
+}
